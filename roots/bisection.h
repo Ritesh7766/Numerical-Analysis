1ac5661e79017e374fc4893d4eb99326f0f50d7d
Ritesh Saha <riteshsaha214@gmail.com>
2021-11-21 11:38:12 +0530

    [ Bisection method added ]

---
+++ roots/bisection.h
@@ -0,0 +1,13 @@
+#include <cmath>
+
+double bisection(double x0, double x1, double (*f)(double), double epsilon) {
+	double prevroot = 0.0, root = 0.0;
+	do {
+		prevroot = root;
+		root = x0 + (x1 - x0) / 2.0;
+		if (f(root) < 0.0) x0 = root;
+		else x1 = root;
+	}
+	while (fabs(root - prevroot) > epsilon);
+	return root;
+}
