b2cc5c120651ba74801302645715a7402061f90c
Ritesh Saha <riteshsaha214@gmail.com>
2021-12-14 19:34:39 +0530

    [ initial commit ]

--- utility/equalIntervalCSVgenerator.cpp
+++ utility/equalIntervalCSVgenerator.cpp
@@ -1,54 +1,51 @@
 /*
 * Author - Ritesh Saha
 * Generating test cases to test various interpolation and extrapolation methods.
-* The function is supplied by the user.
 */
 #include <random>
 #include <iostream>
 #include <fstream>
 #include <string>
 
-using namespace std;
-
-const double UPPER_BOUND = 100.0;
+const double INITIAL_VAL = 0.0;
+const double UPPER_BOUND = 10000.0;
 const double LOWER_BOUND = 0.0;
 
-// Change the fuction defination for other functions
-double f(double x) {
-	return 3.0*x*x*x - 2.0*x*x + 4.0*x - 9.0; // Some random polynomial.
-}
+using namespace std;
 
 int main(int argc, char *argv[]) {
+	// Checking proper usage.
 	if (argc != 4) {
-		cout << "Usage: ./utility/csvGenerator2 %length% %number of csv files% %interval%" << endl;
-		return 0;
+		cout << "Usage: .\\csvGenerator %length% %number of csv files% %interval%" << endl;
+		return 1;
 	}
 
+	double h = atof(argv[3]);
 	int n = atoi(argv[2]);
 	int len = atoi(argv[1]);
-	string file = "./testCases2/";
+	string file = "./intervals_testCases/";
 	string def = "testCase";
 
 	std::uniform_real_distribution<double> unif(LOWER_BOUND, UPPER_BOUND);
    	std::default_random_engine re;
 
-   	try {
-   		for (int j = 0; j < n; j++) {
+	try {
+		for (int j = 0; j < n; j++) {
 			ofstream csv;
 			csv.open(file + def + to_string(j) + ".csv");
 
 			csv << "x" << "," << "f(x)" << endl;
 
 			for (int i = 0; i < len; i++) {
-				double x = unif(re);
-				double y = f(x);
+				double x = INITIAL_VAL + h * i;
+				double y = unif(re);
 				csv << x << "," << y << endl;
 			}
 
 			csv.close();
 		}
-   	}
-   	catch (ofstream::failure e) {
+	}
+	catch (ofstream::failure e) {
 		cout << "Exception opening/reading/closing file\n" << endl;
 	}
 
--- utility/equalIntervalCSVgenerator2.cpp
+++ utility/equalIntervalCSVgenerator2.cpp
@@ -1,17 +1,15 @@
 /*
 * Author - Ritesh Saha
 * Generating test cases to test various interpolation and extrapolation methods.
-* The function is supplied by the user.
 */
-#include <random>
+
 #include <iostream>
 #include <fstream>
 #include <string>
 
-using namespace std;
+const double INITIAL_VAL = 0.0;
 
-const double UPPER_BOUND = 100.0;
-const double LOWER_BOUND = 0.0;
+using namespace std;
 
 // Change the fuction defination for other functions
 double f(double x) {
@@ -19,36 +17,36 @@ double f(double x) {
 }
 
 int main(int argc, char *argv[]) {
+	// Checking proper usage.
 	if (argc != 4) {
-		cout << "Usage: ./utility/csvGenerator2 %length% %number of csv files% %interval%" << endl;
-		return 0;
+		cout << "Usage: .\\csvGenerator %length% %number of csv files% %interval%" << endl;
+		return 1;
 	}
 
+	double h = atof(argv[3]);
 	int n = atoi(argv[2]);
 	int len = atoi(argv[1]);
-	string file = "./testCases2/";
+	string file = "./intervals_testCases2/";
 	string def = "testCase";
+;
 
-	std::uniform_real_distribution<double> unif(LOWER_BOUND, UPPER_BOUND);
-   	std::default_random_engine re;
-
-   	try {
-   		for (int j = 0; j < n; j++) {
+	try {
+		for (int j = 0; j < n; j++) {
 			ofstream csv;
 			csv.open(file + def + to_string(j) + ".csv");
 
 			csv << "x" << "," << "f(x)" << endl;
 
 			for (int i = 0; i < len; i++) {
-				double x = unif(re);
+				double x = INITIAL_VAL + h * i;
 				double y = f(x);
 				csv << x << "," << y << endl;
 			}
 
 			csv.close();
 		}
-   	}
-   	catch (ofstream::failure e) {
+	}
+	catch (ofstream::failure e) {
 		cout << "Exception opening/reading/closing file\n" << endl;
 	}
 
