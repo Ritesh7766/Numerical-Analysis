459e8be3533e262b04fff3228c2924035da93702
Ritesh Saha <riteshsaha214@gmail.com>
2022-01-16 14:40:34 +0530

    [ Initial commit ]

---
+++ generate_test_cases.py
@@ -0,0 +1,22 @@
+from utils.CSVGenerator import write_csv
+from utils.config import MY_CONFIG
+#import numpy as np
+#from concurrent.futures import ThreadPoolExecutor
+
+
+def x_generator(n = None):
+	return MY_CONFIG.LOWER_BOUND + (MY_CONFIG.INTERVAL * n)
+
+def y_generator(x):
+	return np.sin(x)
+
+@write_csv(x_generator, y_generator)
+def write(file_name):
+	return
+
+'''
+with ThreadPoolExecutor() as executer:
+	file_name = 'testcase'
+	for i in range(MY_CONFIG.NO_OF_FILES):
+		executer.submit(write, f'testcase{i}.csv')
+'''
---
+++ roots/.ipynb_checkpoints/secant-checkpoint.h
@@ -0,0 +1,17 @@
+/*
+* Author - Ritesh Saha
+* Secant method (Optimized regula falsi method)
+*/
+
+#include <cmath>
+
+static double secant(double x0, double x1, double (*f)(double), double epsilon) {
+	double prevroot = 0.0, root = 0.0;
+	do {
+		prevroot = root;
+		root = (x0 * f(x1) - x1 * f(x0)) / (f(x1) - f(x0));
+		x1 = root;
+	}
+	while (fabs(prevroot - root) > epsilon);
+	return root;
+}
